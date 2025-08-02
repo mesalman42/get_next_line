836ecc3fd84a9fac3fd1121646761e034d8e3507
mesalman <mesalman@student.42istanbul.com.tr>
2025-08-02 12:07:47 +0300

    bonus added

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mesalman <mesalman@student.42istanbul.com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/21 13:58:37 by mesalman          #+#    #+#             */
-/*   Updated: 2025/07/21 13:59:05 by mesalman         ###   ########.fr       */
+/*   Created: 2025/08/02 11:56:59 by mesalman          #+#    #+#             */
+/*   Updated: 2025/08/02 11:57:09 by mesalman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 5
 # endif
 
 char	*get_next_line(int fd);
